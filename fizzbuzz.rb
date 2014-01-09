abfbd7dc6a30566ace3bd5226c5854218dafe3c4
Fremen DeRuvo <fremenderuvo@gmail.com>
2014-01-08 18:03:29 -0600

    make fb test

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,28 @@
 class FizzBuzz
+  # give it the integer you want to count up to
+  def initialize(integer)
+    @array = []
+    run(integer)
+  end
 
+  def fzbz(val)
+    val%15 == 0 ? "FizzBuzz" : false 
+  end
+ 
+
+  def fizz(val)
+    val%3 == 0 ? "Fizz" : false
+  end
+
+  def buzz(val)
+     val%5 == 0 ? "Buzz" : val
+  end
+
+
+  def run(count_to)
+    count_to.times do |n|
+      @array.push(fzbz(n+1) || fizz(n+1) || buzz(n+1))
+    end
+    @array
+  end 
 end
--- mt.rb
+++ mt.rb
@@ -1,12 +1,31 @@
 require 'minitest/autorun'
 require_relative 'fizzbuzz.rb'
 
-describe FizzBuzz, "Test Fizz Buzz" do
-  before do
-    @FizzBuzz = FizzBuzz.new
+class TestFizzBuzz < MiniTest::Test
+
+
+  def setup
+    @FizzBuzz = FizzBuzz.new(36)
+    @exptect_results = [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz", 26, "Fizz", 28, 29, "FizzBuzz", 31, 32, "Fizz", 34, "Buzz", "Fizz"]
   end
 
-  after do
-    @FizzBuzz.destroy!
+  def test_fizz
+    assert_equal  "Fizz", @FizzBuzz.fizz(3)
+    refute_equal  "Fizz", @FizzBuzz.fizz(22)
   end
+
+  def test_buzz
+    assert_equal "Buzz", @FizzBuzz.buzz(5)
+    refute_equal "Buzz", @FizzBuzz.buzz(4)
+  end
+
+  def test_fzbz
+    assert_equal "FizzBuzz", @FizzBuzz.fzbz(15)
+    refute_equal "FizzBuzz", @FizzBuzz.fzbz(16)
+  end
+
+  def test_run
+    assert_equal @exptect_results, @FizzBuzz.run(36)
+  end
+
 end
