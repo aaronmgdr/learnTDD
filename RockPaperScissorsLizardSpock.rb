9847d2027c3faf1f047fe352a5d0d8d0f37b13d0
Fremen DeRuvo <fremenderuvo@gmail.com>
2014-01-12 21:39:31 -0600

    Implemented working game

--- RockPaperScissorsLizardSpock.rb
+++ RockPaperScissorsLizardSpock.rb
@@ -1,46 +1,126 @@
-class RPSLS
+class RPS
+  attr_reader :person, :computer
+  def initialize(player)
+    #setup randomizor for computer choice
+    rand = Random.new
+    randnum = rand.rand(4)
+    choices = {0 => :spock, 1 => :lizard, 2 => :rock, 3 => :paper, 4 => :scissors }
 
-  attr_accessor :opponant, :hand
-
-  def initialize(hand)
-    possible_hands = { 0 => :scissors, 
-      1 => :paper, 
-      2 => :rock,
-      3 => :lizard,
-      4 => :spock,
-    }
-    @hand = Avatar.new(hand.downcase.to_sym)
-    random = Random.new
-    randnum = random.rand(4)
-    @opponant = Avatar.new(possible_hands[randnum])
+    #invoke avatars
+    avatars = {spock: Spock.new, lizard: Lizard.new, rock: Rock.new, paper: Paper.new, scissors: Scissors.new}     
+    @person = avatars[player.to_sym]
+    @computer = avatars[choices[randnum]]
+    
+    fight(@person, @computer)
   end
 
-#internal
-  
+  def fight(a, b)
+    
+    if a.defeats?(b.to_sym)
+      winner = a
+      loser = b
+    else
+      winner = b
+      loser = a
+    end
+    
+    if winner == @person
+      status = "Win"
+    else 
+      status = "Lose"
+    end
+
+    puts "You #{status}! -- #{winner} #{winner.verb_for(loser.to_sym)} #{loser}"
+  end
+end
+##
+class Avatar
+
+  def to_sym
+    return self.to_s.downcase.to_sym
+  end
+
+end
+##
+class Spock < Avatar
+  def verb_for(loser)
+    verb = {
+      scissors: "smashes",
+      rock: "vaporizes"
+    }
+    return verb[loser]
+  end
+  def defeats?(opponant)
+    (opponant == :rock) || (opponant == :scissors)
+  end
+
+  def to_s
+    return "Spock"
+  end
 end
 
-class Avatar
-  def initalize(type)
-    type
+class Rock < Avatar
+  def verb_for(loser)
+    verb = {
+      scissors: "crushes",
+      lizard: "crushes"
+    }
+    return verb[loser]
+  end
+  def defeats?(opponant)
+    (opponant == :scissors) || (opponant == :lizard)
   end
 
-  def scissor_defeats?(vs)
-    vs == :paper || :lizard ? true : false
+  def to_s
+    return "Rock"
+  end
+end
+
+class Lizard < Avatar
+  def verb_for(loser)
+    verb = {
+      spock: "poisons",
+      paper: "eats"
+    }
+    return verb[loser]
+  end
+  def defeats?(opponant)
+    (opponant == :paper) || (opponant == :spock)
   end
 
-  def paper_defeats?(vs)
-    vs == :rock || :spock ? true : false
+  def to_s
+    return "Lizard"
+  end
+end
+class Scissors < Avatar
+  def verb_for(loser)
+    verb = {
+      lizard: "decapitates",
+      paper: "cuts"
+    }
+    return verb[loser]
+  end
+  def defeats?(opponant)
+    (opponant == :paper) || ( opponant == :lizard)
   end
 
-  def rock_defeats?(vs)
-    vs == :scissors || :lizard ? true : false
+  def to_s
+    return "Scissors"
+  end
+end
+class Paper < Avatar
+  def verb_for(loser)
+    verb = {
+      rock: "covers",
+      spock: "disproves"
+    }
+    return verb[loser]
+  end
+  def defeats?(opponant)
+    (opponant == :rock) || (opponant ==  :spock)
   end
 
-  def lizard_defeats?(vs)
-    vs == :paper || :spock ? true : false
-  end
-
-  def spock_defeats?(vs)
-    vs == :rock || :scissors ? true : false
+  def to_s
+    return "Paper"
   end
 end
--- TestRockPaperScissorsLizardSpock.rb
+++ TestRockPaperScissorsLizardSpock.rb
@@ -1,84 +1,151 @@
 require 'minitest/autorun'
 require_relative 'RockPaperScissorsLizardSpock.rb'
 
-class TestRPSLS < MiniTest::Test
-  def setup
-    @RPSLSGame = RPSLS.new('Scissors')
+class TestRPS < MiniTest::Test
+  def setup 
+    @RPS = RPS.new(:spock)
+    @Computer1 = Paper.new
+    @Computer2 = Rock.new
+    @Computer3 = Lizard.new
   end
 
-  def test_fighters
-    refute_nil(@RPSLSGame.hand)
-    refute_nil(@RPSLSGame.opponant)
+  def test_invoke_two_avatars_on_initialization
+    assert_kind_of Avatar, @RPS.person
+    assert_kind_of Avatar, @RPS.computer
   end
 
-  # def test_opponant_is_sym
-  #   skip assert_kind_of Avatar, @RPSLSGame.opponant
-  # end
-  # def test_hand_is_sym
-  #   skip assert_kind_of Avatar, @RPSLSGame.hand
-  # end
-
-  # def test_winner_is_player
-  #   skip assert_output("You Win") {}
-  # end
-
-  # def test_winner_is_computer
-  #   skip assert_output("You lose") {}
-  # end
-
-  # def test_fight_?
-  #  skip assert_output, @RPSLSGame.fight(@RPSLSGame.hand,@RPSLSGame.opponant)
-  # end
+  def test_fight
+    assert_equal "You Lose! -- Paper disproves Spock", @RPS.fight(@RPS.person, @Computer1)
+    assert_equal "You Win! -- Spock vaporizes Rock", @RPS.fight(@RPS.person, @Computer2)
+    assert_equal "You Lose! -- Lizard poisons Spock", @RPS.fight(@RPS.person, @Computer3)
+  end
 end
 
-class TestAvatar< MiniTest::Test
+class TestSpock < MiniTest::Test
   def setup
-    @Spock = Avatar.new(:spock)
-  end
-  
-  def test_defeats?
-  
-  end
-  
-  def test_winning_messege
-
+    @spock = Spock.new
   end
 
-  def test_scissor_defeats?
-    assert_equal true, @RPSLSGame.scissor_defeats?(:paper)
-    assert_equal true, @RPSLSGame.scissor_defeats?(:lizard)
+  def test_sym
+    assert_equal :spock, @spock.to_sym 
   end
 
-  def test_paper_defeats?
-    assert_equal true, @RPSLSGame.paper_defeats?(:rock)
-    assert_equal true, @RPSLSGame.paper_defeats?(:srock)
+  def test_verb_for
+    assert_equal "smashes", @spock.verb_for(:scissors) 
+    assert_equal "vaporizes", @spock.verb_for(:rock) 
   end
 
-  def test_rock_defeats?
-    assert_equal true, @RPSLSGame.rock_defeats?(:lizard)
-    assert_equal true, @RPSLSGame.rock_defeats?(:scissors)
+  def test_defeat_scissors
+    assert_equal true, @spock.defeats?(:scissors)
   end
 
-  def test_lizard_defeats?
-    assert_equal true, @RPSLSGame.lizard_defeats?(:spock)
-    assert_equal true, @RPSLSGame.lizard_defeats?(:paper)
+  def test_defeats_rock
+    assert_equal true, @spock.defeats?(:rock)
   end
 
-  def test_spock_defeats?
-    assert_equal true, @RPSLSGame.spock_defeats?(:scissors)
-    assert_equal true, @RPSLSGame.spock_defeats?(:rock)
+  def test_does_not_defeat_paper
+    refute_equal true, @spock.defeats?(:paper) 
   end
-
 end
 
+class TestRock < MiniTest::Test
+  def setup
+    @rock = Rock.new
+  end
 
-# Scissors cut paper
-# Paper covers rock
-# Rock crushes lizard
-# Lizard poisons Spock
+  def test_sym
+    assert_equal :rock, @rock.to_sym 
+  end
+
+  def test_verb_for
+    assert_equal "crushes", @rock.verb_for(:scissors) 
+    assert_equal "crushes", @rock.verb_for(:lizard) 
+  end
+
+  def test_defeats
+    assert_equal true, @rock.defeats?(:scissors)
+    assert_equal true, @rock.defeats?(:lizard)
+    refute_equal true, @rock.defeats?(:paper) 
+
+  end
+end
+class TestLizard < MiniTest::Test
+  def setup
+    @Lizard = Lizard.new
+  end
+
+  def test_sym
+    assert_equal :lizard, @Lizard.to_sym 
+  end
+
+  def test_verb_for
+    assert_equal "poisons", @Lizard.verb_for(:spock) 
+    assert_equal "eats", @Lizard.verb_for(:paper) 
+  end
+
+  def test_defeats
+    assert_equal true, @Lizard.defeats?(:spock)
+    assert_equal true, @Lizard.defeats?(:paper)
+    refute_equal true, @Lizard.defeats?(:scissors) 
+ 
+  end
+end
+
+class TestScissors < MiniTest::Test
+  def setup
+    @Scissors = Scissors.new
+  end
+
+  def test_sym
+    assert_equal :scissors, @Scissors.to_sym 
+  end
+
+  def test_verb_for
+    assert_equal "cuts", @Scissors.verb_for(:paper) 
+    assert_equal "decapitates", @Scissors.verb_for(:lizard) 
+  end
+
+  def test_defeats
+    assert_equal true, @Scissors.defeats?(:paper) 
+    assert_equal true, @Scissors.defeats?(:lizard)
+    refute_equal true, @Scissors.defeats?(:scissors) 
+  end
+end
+
+class TestPaper < MiniTest::Test
+  def setup
+    @Paper = Paper.new
+  end
+
+  def test_sym
+    assert_equal :paper, @Paper.to_sym 
+  end
+
+  def test_verb_for
+    assert_equal "covers", @Paper.verb_for(:rock) 
+    assert_equal "disproves", @Paper.verb_for(:spock) 
+  end
+
+  def test_defeats
+    assert_equal true, @Paper.defeats?(:rock) 
+    assert_equal true, @Paper.defeats?(:spock)
+    refute_equal true, @Paper.defeats?(:scissors) 
+  end
+end
+
 # Spock smashes scissors
-# Scissors decapitate lizard
-# Lizard eats paper
-# Paper disproves Spock
 # Spock vaporizes rock
 # Rock crushes scissors
+# Rock crushes lizard
+# Lizard poisons Spock
+# Lizard eats paper
+
+# Scissors cut paper
+# Scissors decapitate lizard
+# Paper covers rock
+# Paper disproves Spock
+
+
+
+
+
